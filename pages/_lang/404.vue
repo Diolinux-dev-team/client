dacb99b021912bf6d521112b5ecafa0146f7a139
Gerardo Junior <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-24 17:40:25 -0300

    Create 404 pages

---
+++ pages/_lang/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Content">
+    <div class="container">
+      <h1 class="Content__Title">{{ $t('notfound.title') }}</h1>
+      <p>{{ $t('notfound.text') }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return { title: this.$t('notfound.title') }
+  }
+}
+</script>
