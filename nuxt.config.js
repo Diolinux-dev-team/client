a1de75ab9e015ead69dadd61fbc402b5cf9192a5
Gerardo Junior <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-24 16:12:53 -0300

    Disable indexing

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Diolinux plus' },
-      { name='robots', content='noindex' }
+      { name:'robots', content:'noindex' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
